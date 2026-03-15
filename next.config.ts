14030f6de02bce0f0755e2a91398ff4d93c56b4c
mycampushub <mycampuslifestyle@gmail.com>
2026-03-15 23:59:10 +0600

    standalone removed

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: "/fleet-management",
   assetPrefix: "/fleet-management",
-  output: "standalone",
   images: {
     unoptimized: true,
   },
