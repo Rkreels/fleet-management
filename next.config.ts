be2b8c9034ce5bbd6971170e094353147ebc9731
mycampushub <mycampuslifestyle@gmail.com>
2026-03-15 23:48:32 +0600

    standalone fix build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: "/fleet-management",
   assetPrefix: "/fleet-management",
+  output: "standalone",
   images: {
     unoptimized: true,
   },
